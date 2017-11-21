e76ad308ccebd645f2e9feb3d09567e11638b718
Ryan Devlin <ryandevlin86@gmail.com>
2017-11-20 20:41:57 -0500

    No foreign keys because prod is being stupid

--- db/migrate/20171121002843_add_foreign_keys_to_line_items.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeysToLineItems < ActiveRecord::Migration[5.1]
-  def change
-    add_foreign_key :line_items, :products, primary_key: 'productCode'
-    add_foreign_key :line_items, :orders, primary_key: 'orderNumber'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121002843) do
+ActiveRecord::Schema.define(version: 20171120062337) do
 
   create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.datetime "created_at", null: false
@@ -136,8 +136,6 @@ ActiveRecord::Schema.define(version: 20171121002843) do
   add_foreign_key "employees", "employees", column: "reportsTo", primary_key: "employeeNumber", name: "employees_ibfk_1"
   add_foreign_key "employees", "offices", column: "officeCode", primary_key: "officeCode", name: "employees_ibfk_2"
   add_foreign_key "line_items", "carts"
-  add_foreign_key "line_items", "orders", primary_key: "orderNumber"
-  add_foreign_key "line_items", "products", primary_key: "productCode"
   add_foreign_key "orderdetails", "orders", column: "orderNumber", primary_key: "orderNumber", name: "orderdetails_ibfk_1"
   add_foreign_key "orderdetails", "products", column: "productCode", primary_key: "productCode", name: "orderdetails_ibfk_2"
   add_foreign_key "orders", "customers", column: "customerNumber", primary_key: "customerNumber", name: "orders_ibfk_1"
