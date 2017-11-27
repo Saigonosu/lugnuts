fd253f48f2b6eae051c2c4b43b239462ab63e0ed
Ryan Devlin <ryandevlin86@gmail.com>
2017-11-27 00:12:50 -0500

    Add Order History
    From User edit, the user will now have a link to take them to their
    order history. We'll utilize the path /orders that was already created,
    rather than grabbing all orders, we'll only grab orders for current_user
    
    Also added a #total method on Order and OrderDetails, so we can cleanly
    grab the totals for those resources

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,10 @@ class Order < ApplicationRecord
   belongs_to :customer, class_name: 'Customer', foreign_key: 'customerNumber'
   has_many :orderdetails, foreign_key: 'orderNumber'
   has_many :products, through: :orderdetails, class_name: 'Product'
+
+  def total
+    orderdetails.inject(0) do |sum, detail|
+      sum += detail.total
+    end
+  end
 end
--- app/models/orderdetail.rb
+++ app/models/orderdetail.rb
@@ -2,4 +2,8 @@ class Orderdetail < ApplicationRecord
   self.primary_key = 'orderNumber', 'productCode'
   belongs_to :product, class_name: 'Product', foreign_key: 'productCode'
   belongs_to :order, class_name: 'Order', foreign_key: 'orderNumber'
+
+  def total
+    priceEach * quantityOrdered
+  end
 end
