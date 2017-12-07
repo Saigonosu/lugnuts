cd5f6de5f643ef78e163b00b1aaaa4bf878f1501
Ryan Devlin <ryandevlin86@gmail.com>
2017-12-06 23:29:07 -0500

    Fixed Typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY']
-    aws_secret_access_key: ENV['SECRET_KEY'],
+    aws_access_key_id: ENV['ACCESS_KEY'],
+    aws_secret_access_key: ENV['SECRET_KEY']
   }
   config.fog_directory = 'lugnuts'
   config.fog_public = 'false'
