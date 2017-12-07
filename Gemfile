451f9e800550b77ca61aff3dd19b5c13dc319c1d
Ryan Devlin <ryandevlin86@gmail.com>
2017-12-06 23:20:56 -0500

    Add CarrierWave For Image Upload
    Required adding fog-aws gem as well

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 #
 gem 'stripe'
+gem 'carrierwave'
+gem 'fog-aws'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY']
+    aws_secret_access_key: ENV['SECRET_KEY'],
+  }
+  config.fog_directory = 'lugnuts'
+  config.fog_public = 'false'
+end
